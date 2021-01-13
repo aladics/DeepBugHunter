d03c658583eb9b924c259d0726b44fbf559b83f6
Aladics Tamas <aladics@inf.u-szeged.hu>
2021-01-13 16:02:10 +0100

    Initial commit for migrating to tf2

--- metrics.py
+++ metrics.py
@@ -1,25 +1,45 @@
 import tensorflow as tf
+import tensorflow.keras.metrics as tf_metrics 
 
 # Matthew's correlation coefficient
 def mcc(labels, preds):
-    tp, tp_update = tf.metrics.true_positives(labels, preds)
-    tn, tn_update = tf.metrics.true_negatives(labels, preds)
-    fp, fp_update = tf.metrics.false_positives(labels, preds)
-    fn, fn_update = tf.metrics.false_negatives(labels, preds)
+    tp_m = tf_metrics.TruePositives()
+    fp_m = tf_metrics.FalsePositives()
+    tn_m = tf_metrics.TrueNegatives()
+    fn_m = tf_metrics.FalseNegatives()
+
+    tp_m.update_state(labels, preds)
+    tn_m.update_state(labels, preds)
+    fp_m.update_state(labels, preds)
+    fn_m.update_state(labels, preds)
+    
+    tp = tp_m.result()
+    tn = tn_m.result()
+    fp = fp_m.result()
+    fn = fn_m.result()
     eps = 1e-7
 
     num = (tp * tn - fp * fn)
     denom = tf.sqrt((tp + fp)*(tp + fn)*(tn + fp)*(tn + fn)) + eps
 
-    return (num / denom), tf.group(tp_update, tn_update, fp_update, fn_update)
+    return num / denom
 
 # F-Measure
 def fmes(labels, preds):
-    precision, precision_update = tf.metrics.precision(labels, preds)
-    recall, recall_update = tf.metrics.recall(labels, preds)
+    precision_m =  tf_metrics.Precision()
+    recall_m = tf_metrics.Recall()
+
+    precision_m.update_state(labels, preds)
+    recall_m.update_state(labels, preds)
+    
+    precision = precision_m.result()
+    recall = recall_m.result()
+
+    """ precision, precision_update = tf.compat.v1.metrics.precision(labels, preds)
+    recall, recall_update = tf.compat.v1.metrics.recall(labels, preds) """
     eps = 1e-7
 
     num = (2 * precision * recall)
     denom = precision + recall + eps
 
-    return (num / denom), tf.group(precision_update, recall_update)
+    return num / denom
