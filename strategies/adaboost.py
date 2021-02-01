4c3f441320393fc56ad4dd11ee0dd58054fdd804
Aladics Tamas <aladics@inf.u-szeged.hu>
2021-02-01 19:47:14 +0100

    Added additional parameters for AdaBoost

--- strategies/adaboost.py
+++ strategies/adaboost.py
@@ -2,11 +2,14 @@ import argparse
 import dbh_util as util
 
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 parser = argparse.ArgumentParser()
 
-arser.add_argument('--n-estimators', type=int, default=10, help='Maximum number of estimators at which boosting is terminated.')
-parser.add_argument('--learning-rate', type=int, default=1, help='Shrinks the contribution of each classifier')
+parser.add_argument('--n-estimators', type=int, default=10, help='Maximum number of trees at which boosting is terminated.')
+parser.add_argument('--learning-rate', type=float, default=1, help='Shrinks the contribution of each tree')
+parser.add_argument('--max-depth', type=int, default = 1, help='Max depth of each tree')
+parser.add_argument('--min-samples-leaf', type=int, default=1, help='Minimum number of samples at a leaf node')
 
 
 def predict(classifier, test, args, sargs_str, threshold=None):
@@ -19,4 +22,5 @@ def predict(classifier, test, args, sargs_str, threshold=None):
 
 def learn(train, dev, test, args, sargs_str):
     sargs = util.parse(parser, sargs_str.split())
-    return util.sklearn_wrapper(train, dev, test, AdaBoostClassifier(**sargs))
+    base_classifier = DecisionTreeClassifier(max_depth = sargs.pop('max_depth'), min_samples_leaf = sargs.pop('min_samples_leaf'))
+    return util.sklearn_wrapper(train, dev, test, AdaBoostClassifier(base_estimator = base_classifier, **sargs))
